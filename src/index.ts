c19a76bfc2eba5613b669752e5e6378521a974b2
Antônio Hugo <hugorplobo@gmail.com>
2023-04-14 20:08:05 -0300

    feat: register routes and schemas on server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { config } from "dotenv";
 import Fastify from "fastify";
+import { schemas } from "./adapters/controllers/schemas/buildSchemas";
+import { registerRoute } from "./server/routes/register";
 
 config();
 
 const server = Fastify({
   logger: true
 });
+
+for (const schema of schemas) {
+  server.addSchema(schema);
+}
+
+server.register(registerRoute);
 
 server.listen({ port: Number(process.env.PORT || 8080) }, (err, addr) => {
   if (err) {
