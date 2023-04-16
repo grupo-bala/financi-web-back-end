b70b5fbaf7a1e4b47b1a2ae58438a6268dc71fee
Antônio Hugo <hugorplobo@gmail.com>
2023-04-15 22:29:23 -0300

    refactor: add optional hash option

--- src/model/data/password.ts
+++ src/model/data/password.ts
@@ -3,8 +3,12 @@ import { Hash } from "../../adapters/services/hash";
 export class Password {
   private readonly password: string;
 
-  constructor(password: string) {
-    this.password = Hash.hash(password);
+  constructor(password: string, isHashed = false) {
+    if (isHashed) {
+      this.password = password
+    } else {
+      this.password = Hash.hash(password);
+    }
   }
 
   get value(): string {
