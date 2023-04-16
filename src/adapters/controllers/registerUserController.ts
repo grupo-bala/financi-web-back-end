c1fb7f2527a61028beb9c8e927ec18dbc8162ccc
Antônio Hugo <hugorplobo@gmail.com>
2023-04-15 22:31:32 -0300

    refactor: add error handling

--- src/adapters/controllers/registerUserController.ts
+++ src/adapters/controllers/registerUserController.ts
@@ -3,28 +3,37 @@ import { RegisterUserInput } from "./schemas/registerUserSchema";
 import { User } from "../../model/user";
 import { Decimal } from "@prisma/client/runtime/library";
 import { Email } from "../../model/data/email";
-import { RegisterUserOnDb } from "../../usecases/registerUserOnDb";
+import { RegisterUser } from "../../usecases/registerUser";
 import { Password } from "../../model/data/password";
+import { StatusCodes } from "http-status-codes";
 
 export class RegisterUserController {
-  private readonly registerUser: RegisterUserOnDb;
+  private readonly registerUser: RegisterUser;
 
-  constructor(registerUser: RegisterUserOnDb) {
+  constructor(registerUser: RegisterUser) {
     this.registerUser = registerUser;
   }
 
-  async handle(request: FastifyRequest, _reply: FastifyReply) {
+  async handle(request: FastifyRequest, reply: FastifyReply) {
     const body = request.body as RegisterUserInput;
-    const newUser = new User(
-      -1,
-      body.name,
-      body.username,
-      new Decimal(body.fixedIncome),
-      new Decimal(0),
-      new Email(body.email),
-      new Password(body.password)
-    );
     
-    await this.registerUser.registerUser(newUser);
+    try {
+      const newUser = new User(
+        -1,
+        body.name,
+        body.username,
+        new Decimal(body.fixedIncome),
+        new Decimal(0),
+        new Email(body.email),
+        new Password(body.password)
+      );
+      
+      await this.registerUser.registerUser(newUser);
+      await reply.status(StatusCodes.CREATED).send();
+    } catch (error) {
+      await reply
+        .status(StatusCodes.CONFLICT)
+        .send({ msg: (error as Error).message });
+    }
   }
 }
