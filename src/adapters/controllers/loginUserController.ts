574db62f94e771a6f0cabc2c4d14332fe3ecbd8f
Antônio Hugo <hugorplobo@gmail.com>
2023-04-15 22:32:29 -0300

    feat: add login user controller

---
+++ src/adapters/controllers/loginUserController.ts
@@ -0,0 +1,27 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { LoginUser } from "../../usecases/loginUser";
+import { LoginUserInput } from "./schemas/loginUserSchema";
+import { StatusCodes } from "http-status-codes";
+import { Password } from "../../model/data/password";
+
+export class LoginUserController {
+  private readonly loginUser: LoginUser;
+
+  constructor(loginUser: LoginUser) {
+    this.loginUser = loginUser;
+  }
+
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const query = request.query as LoginUserInput;
+
+    try {
+      const jwt = await this.loginUser.loginUser(query.username, new Password(query.password));
+      await reply
+        .send({ msg: jwt.value });
+    } catch (error) {
+      await reply
+        .status(StatusCodes.UNAUTHORIZED)
+        .send({ msg: (error as Error).message });
+    }
+  }
+}
