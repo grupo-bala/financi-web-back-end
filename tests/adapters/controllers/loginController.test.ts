7985b8abf1b3db7a8823cf58fcdc8386fd3ac528
Antônio Hugo <hugorplobo@gmail.com>
2023-04-27 22:13:26 -0300

    test: add login and register controllers tests

---
+++ tests/adapters/controllers/loginController.test.ts
@@ -0,0 +1,58 @@
+import Fastify from "fastify";
+import { registerHandlers } from "../../../src/server/plugins/registerHandlers";
+import { LoginUser } from "../../../src/usecases/loginUser";
+import { Token } from "../../../src/adapters/data/token";
+import { Password } from "../../../src/model/data/password";
+import { mock } from "../../util";
+
+const server = Fastify();
+jest.mock("../../../src/usecases/loginUser.ts");
+
+describe("testes do controller de login", () => {
+  beforeAll(() => {
+    server.register(registerHandlers);
+  });
+
+  test("caso de uso sem erros deve retornar status code 200 e um token não vazio", async () => {
+    mock(LoginUser).mockImplementation(() => {
+      return {
+        loginUser: async (_: string, __: Password) => Token.encode("test", false),
+      }
+    });
+
+    const res = await server.inject({
+      method: "GET",
+      url: `http://localhost:${process.env.PORT}/login`,
+      query: {
+        username: "test",
+        password: "test"
+      }
+    });
+
+    const body = await JSON.parse(res.body);
+
+    expect(res.statusCode).toBe(200);
+    expect(body.msg).not.toBe("");
+  });
+
+  test("erro no caso de uso deve retornar status code 401", async () => {
+    mock(LoginUser).mockImplementation(() => {
+      return {
+        loginUser: async (_: string, __: Password) => {
+          throw new Error("");
+        },
+      }
+    });
+
+    const res = await server.inject({
+      method: "GET",
+      url: `http://localhost:${process.env.PORT}/login`,
+      query: {
+        username: "test",
+        password: "test"
+      }
+    });
+
+    expect(res.statusCode).toBe(401);
+  });
+});
