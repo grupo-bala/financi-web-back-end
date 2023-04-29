53883af732618c7d107619af7849152fa0fac0aa
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-29 18:16:19 -0300

    feat: update build schemas with get news schema

--- src/adapters/controllers/schemas/buildSchemas.ts
+++ src/adapters/controllers/schemas/buildSchemas.ts
@@ -2,9 +2,11 @@ import { buildJsonSchemas } from "fastify-zod";
 import { registerUserSchema } from "./registerUserSchema";
 import { loginUserSchema } from "./loginUserSchema";
 import { addNewsSchema } from "./news/addSchema";
+import { getNewsSchema } from "./news/getNewsSchema";
 
 export const { schemas, $ref } = buildJsonSchemas({
   registerUserSchema,
   loginUserSchema,
   addNewsSchema,
+  getNewsSchema,
 });
