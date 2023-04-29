405daadead4d9e75d227bf05869da0d4ca2e510a
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-29 17:24:45 -0300

    feat: implement an add news controller

---
+++ src/adapters/controllers/news/addController.ts
@@ -0,0 +1,24 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { AddNewsInput } from "../schemas/news/addSchema";
+import { StatusCodes } from "http-status-codes";
+import { AddNews } from "../../../usecases/news/addNews";
+import { PostgresNewsRepository } from "../../repositories/postgresNewsRepository";
+import { News } from "../../../model/news";
+
+export class AddNewsController {
+  private readonly addNews = new AddNews(new PostgresNewsRepository());
+
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const { author, content, imgURL, publishDate, summary, title } = request.body as AddNewsInput;
+
+    try {
+      await this.addNews.add(new News(author, title, summary, content, publishDate, imgURL));
+
+      await reply.status(StatusCodes.CREATED).send();
+    } catch (error) {
+      await reply
+        .status(StatusCodes.CONFLICT)
+        .send({ msg: (error as Error).message });
+    }
+  }
+}
