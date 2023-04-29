5361243c1521063d9a0203fcc6fb52db36c775c7
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-29 20:11:01 -0300

    feat: implement get news controller

---
+++ src/adapters/controllers/news/getNewsController.ts
@@ -0,0 +1,20 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { StatusCodes } from "http-status-codes";
+import { GetNews } from "../../../usecases/news/getNews";
+import { PostgresNewsRepository } from "../../repositories/postgresNewsRepository";
+import { GetNewsInput } from "../schemas/news/getNewsSchema";
+
+export class GetNewsController {
+  private readonly getNews = new GetNews(new PostgresNewsRepository()); // todo
+
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const { page, size } = request.query as GetNewsInput;
+    
+    const news = await this.getNews.getNews(page, size);
+
+    await reply.status(StatusCodes.OK).send({
+      data: news.news,
+      pages: news.howManyPages,
+    });
+  }
+}
