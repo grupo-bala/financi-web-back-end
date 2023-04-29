eb7516d80da2e9d987b66e02bc1011b5b8bbdbb6
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-29 17:24:11 -0300

    feat: implement news repository for the add news use case

---
+++ src/adapters/repositories/postgresNewsRepository.ts
@@ -0,0 +1,34 @@
+import { News } from "../../model/news";
+import { NewsRepository } from "../../usecases/news/interfaces/NewsRepository";
+import { PrismaHelper } from "./prismaHelper";
+
+export class PostgresNewsRepository implements NewsRepository {
+  async exists(title: string): Promise<boolean> {
+    const count = await new PrismaHelper()
+      .client
+      .news
+      .count({
+        where: {
+          title,
+        },
+      });
+
+    return count > 0;
+  }
+
+  async add({author, content, imgURL, publishDate, summary, title}: News): Promise<void> {
+    await new PrismaHelper()
+      .client
+      .news
+      .create({
+        data: {
+          author,
+          content,
+          img_url: imgURL,
+          publish_date: publishDate,
+          summary,
+          title,
+        },
+      });
+  }
+}
