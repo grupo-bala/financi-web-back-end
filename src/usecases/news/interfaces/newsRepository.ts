e516db69a9241beae4e953322853e54213b9088e
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-29 20:09:43 -0300

    feat: update news repository interface with get news methods

--- src/usecases/news/interfaces/newsRepository.ts
+++ src/usecases/news/interfaces/newsRepository.ts
@@ -3,4 +3,6 @@ import { News } from "../../../model/news";
 export interface NewsRepository {
   exists: (title: string) => Promise<boolean>;
   add: (news: News) => Promise<void>;
+  getNews: (page: number, size: number) => Promise<News[]>;
+  getSize: () => Promise<number>;
 }
