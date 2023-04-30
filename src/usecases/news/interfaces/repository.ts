9cd16720cf316cdbe4ccba86ac994b444c74557a
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-30 16:04:01 -0300

    feat: update news repository interface with update and exists by id

--- src/usecases/news/interfaces/repository.ts
+++ src/usecases/news/interfaces/repository.ts
@@ -2,9 +2,11 @@ import { News } from "../../../model/news";
 
 export interface NewsRepository {
   exists: (title: string) => Promise<boolean>;
+  existsById: (id: number) => Promise<boolean>;
   add: (news: News) => Promise<void>;
   getAll: (page: number, size: number) => Promise<News[]>;
   getSize: () => Promise<number>;
   remove: (title: string) => Promise<void>;
   get: (title: string) => Promise<News>;
+  update: (news: News) => Promise<void>;
 }
