a7c7a60dfceb1efaee36eb07825a43167b3e29b2
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-30 16:20:29 -0300

    feat: update news repository interface methods to work with id

--- src/usecases/news/interfaces/repository.ts
+++ src/usecases/news/interfaces/repository.ts
@@ -1,12 +1,12 @@
 import { News } from "../../../model/news";
 
 export interface NewsRepository {
-  exists: (title: string) => Promise<boolean>;
+  existsByTitle: (title: string) => Promise<boolean>;
   existsById: (id: number) => Promise<boolean>;
   add: (news: News) => Promise<void>;
   getAll: (page: number, size: number) => Promise<News[]>;
   getSize: () => Promise<number>;
-  remove: (title: string) => Promise<void>;
-  get: (title: string) => Promise<News>;
+  remove: (id: number) => Promise<void>;
+  get: (id: number) => Promise<News>;
   update: (news: News) => Promise<void>;
 }
