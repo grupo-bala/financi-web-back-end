e580d961498d914cc3dc04af3da7b4e4604d4c8e
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-29 21:00:29 -0300

    feat: implement remove news controller

---
+++ src/adapters/controllers/news/removeController.ts
@@ -0,0 +1,23 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { RemoveNews } from "../../../usecases/news/removeNews";
+import { PostgresNewsRepository } from "../../repositories/postgresNewsRepository";
+import { RemoveNewsInput } from "../schemas/news/removeSchema";
+import { StatusCodes } from "http-status-codes";
+
+export class RemoveNewsController {
+  private readonly removeNews = new RemoveNews(new PostgresNewsRepository());
+
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const { title } = request.body as RemoveNewsInput;
+
+    try {
+      await this.removeNews.remove(title);
+
+      await reply.status(StatusCodes.OK).send();
+    } catch (error) {
+      await reply
+        .status(StatusCodes.NOT_FOUND)
+        .send({ msg: (error as Error).message });
+    }
+  }
+}
