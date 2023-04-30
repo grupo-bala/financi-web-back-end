92b6e110c10f407f86b05bf5ab744840616968ab
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-30 16:05:44 -0300

    feat: implement update news use case

---
+++ src/usecases/news/update.ts
@@ -0,0 +1,20 @@
+import { News } from "../../model/news";
+import { NewsRepository } from "./interfaces/repository";
+
+export class UpdateNews {
+  private readonly newsRepository: NewsRepository;
+
+  constructor(newsRepository: NewsRepository) {
+    this.newsRepository = newsRepository;
+  }
+
+  async update(news: News): Promise<void> {
+    if (!(await this.newsRepository.existsById(news.id!))) {
+      throw new Error("Essa notícia não existe");
+    } else if (!(await this.newsRepository.exists(news.title))) {
+      throw new Error("Uma notícia com esse nome já existe");
+    }
+
+    await this.newsRepository.update(news);
+  }
+}
