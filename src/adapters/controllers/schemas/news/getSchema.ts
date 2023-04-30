17b12df9acafb421a6fad24f1650ac5af2946c80
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-30 16:19:35 -0300

    feat: update get news and remove schemas to request an id

--- src/adapters/controllers/schemas/news/getSchema.ts
+++ src/adapters/controllers/schemas/news/getSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const getNewsSchema = z.object({
-  title: z.string(),
+  id: z.number(),
 });
 
 export type GetNewsInput = z.infer<typeof getNewsSchema>;
--- src/adapters/controllers/schemas/news/removeSchema.ts
+++ src/adapters/controllers/schemas/news/removeSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const removeNewsSchema = z.object({
-  title: z.string(),
+  id: z.number(),
 });
 
 export type RemoveNewsInput = z.infer<typeof removeNewsSchema>;
