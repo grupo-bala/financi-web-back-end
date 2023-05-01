1b75df27fff72b04b9fa0a11815a9d95827521ea
Antônio Hugo <hugorplobo@gmail.com>
2023-05-01 19:36:00 -0300

    refactor: update user repository interface

--- src/usecases/interfaces/userRepository.ts
+++ src/usecases/interfaces/userRepository.ts
@@ -2,6 +2,6 @@ import { User } from "../../model/user";
 
 export interface UserRepository {
   exists: (username: string) => Promise<boolean>;
-  add: (user: User) => Promise<void>;
+  add: (user: User) => Promise<User>;
   getByUsername: (username: string) => Promise<User>;
 }
