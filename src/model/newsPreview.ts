a4e8f88cab794fa9b1308ac532e7fe213373fe41
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-02 01:43:10 -0300

    refactor: change img to imgURL in news preview model to match the news model property

--- src/model/newsPreview.ts
+++ src/model/newsPreview.ts
@@ -1,13 +1,13 @@
 export class NewsPreview {
   readonly id: number;
-  readonly img: string;
+  readonly imgURL: string;
   readonly title: string;
   readonly publishDate: Date;
 
-  constructor(id: number, title: string, img: string, publishDate: Date) {
+  constructor(id: number, title: string, imgURL: string, publishDate: Date) {
     this.id = id;
     this.title = title;
-    this.img = img;
+    this.imgURL = imgURL;
     this.publishDate = publishDate;
   }
 }
