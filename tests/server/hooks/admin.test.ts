6ead457850fb7d62ddf7a967bdc38ac9926c09fb
Antônio Hugo <hugorplobo@gmail.com>
2023-05-06 00:10:15 -0300

    test: add tests for admin and auth hooks

---
+++ tests/server/hooks/admin.test.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { StatusCodes } from "http-status-codes";
+import { Token } from "../../../src/adapters/data/token";
+import { EnviromentVars } from "../../../src/server/config/enviromentVars";
+import { verifyIsAdmin } from "../../../src/server/hooks/admin";
+
+jest.mock("../../../src/server/config/enviromentVars.ts");
+Object.defineProperty(
+  EnviromentVars,
+  "vars",
+  { get: () => ({ SECRET_KEY: "test" }) },
+);
+
+describe("testes do hook de admin", () => {
+  test("token sem autorização deve falhar", async () => {
+    const token = Token.encode("test", false);
+    const request = {
+      headers: {
+        authorization: `Bearer ${token.encoded}`,
+      },
+    } as any;
+
+    const response = {
+      statusCode: StatusCodes.OK,
+      status(code: number) {
+        this.statusCode = code;
+        return this;
+      },
+      send() {
+        return this;
+      },
+    } as any;
+
+    await verifyIsAdmin(request, response);
+
+    expect(response.statusCode)
+      .toBe(StatusCodes.UNAUTHORIZED);
+  });
+
+  test("token autorizado deve passar", async () => {
+    const token = Token.encode("test", true);
+    const request = {
+      headers: {
+        authorization: `Bearer ${token.encoded}`,
+      },
+    } as any;
+
+    const response = {
+      statusCode: StatusCodes.OK,
+      status(code: number) {
+        this.statusCode = code;
+        return this;
+      },
+      send() {
+        return this;
+      },
+    } as any;
+
+    await verifyIsAdmin(request, response);
+
+    expect(response.statusCode)
+      .toBe(StatusCodes.OK);
+  });
+});
---
+++ tests/server/hooks/auth.test.ts
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { StatusCodes } from "http-status-codes";
+import { validateJWT } from "../../../src/server/hooks/auth";
+import { EnviromentVars } from "../../../src/server/config/enviromentVars";
+
+jest.mock("../../../src/server/config/enviromentVars.ts");
+Object.defineProperty(
+  EnviromentVars,
+  "vars",
+  { get: () => ({ SECRET_KEY: "test" }) },
+);
+
+describe("testes do hook de autenticação", () => {
+  test("cabeçalho sem token deve falhar", async () => {
+    const request = {
+      headers: {},
+    } as any;
+
+    const response = {
+      statusCode: StatusCodes.OK,
+      status(code: number) {
+        this.statusCode = code;
+        return this;
+      },
+      send() {
+        return this;
+      },
+    } as any;
+
+    await validateJWT(request, response);
+
+    expect(response.statusCode)
+      .toBe(StatusCodes.UNAUTHORIZED);
+  });
+
+  test("token inválido deve falhar", async () => {
+    const request = {
+      headers: {
+        authorization: "invalid",
+      },
+    } as any;
+
+    const response = {
+      statusCode: StatusCodes.OK,
+      status(code: number) {
+        this.statusCode = code;
+        return this;
+      },
+      send() {
+        return this;
+      },
+    } as any;
+
+    await validateJWT(request, response);
+
+    expect(response.statusCode)
+      .toBe(StatusCodes.UNAUTHORIZED);
+  });
+});
