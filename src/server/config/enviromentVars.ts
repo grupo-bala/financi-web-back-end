af358f4f458a3c596f986db2bb36872e81527ca2
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-05 23:07:52 -0300

    refactor: remove unnecessary dotenv config from enviroment vars validate function

--- src/server/config/enviromentVars.ts
+++ src/server/config/enviromentVars.ts
@@ -1,6 +1,3 @@
-import { config } from "dotenv";
-import path from "path";
-
 const envKeys = ["PORT", "DATABASE_URL", "SECRET_KEY"] as const;
 type EnvVars = typeof envKeys[number];
 
@@ -9,11 +6,6 @@ export class EnviromentVars {
   private static envVars = {} as { [ K in EnvVars ]: string };
 
   static validateVars() {
-    config({
-      override: true,
-      path: path.resolve(__dirname, "../../../.env"),
-    });
-
     for (const env of envKeys) {
       const value = process.env[env];
 
