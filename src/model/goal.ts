6be4d60f93ec522a5d7990eba85399463ce375d0
Antônio Hugo <hugorplobo@gmail.com>
2023-05-09 08:58:25 -0300

    feat: add goal model

---
+++ src/model/goal.ts
@@ -0,0 +1,33 @@
+import { Decimal } from "@prisma/client/runtime/library";
+
+export class Goal {
+  readonly id: number;
+  readonly title: string;
+  readonly userId: number;
+  readonly totalValue: Decimal;
+  readonly currentValue: Decimal;
+  readonly deadline: Date;
+
+  constructor({
+    id,
+    title,
+    userId,
+    totalValue,
+    currentValue,
+    deadline,
+  }: {
+    id: number,
+    title: string,
+    userId: number,
+    totalValue: Decimal,
+    currentValue: Decimal,
+    deadline: Date,
+  }) {
+    this.id = id;
+    this.title = title;
+    this.userId = userId;
+    this.totalValue = totalValue;
+    this.currentValue = currentValue;
+    this.deadline = deadline;
+  }
+}
