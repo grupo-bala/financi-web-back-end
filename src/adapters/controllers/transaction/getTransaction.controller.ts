27800e1f42bcf6c686f499607ed95dd6c2e5d84c
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-13 20:54:58 -0300

    feat: implement the get transaction controller

---
+++ src/adapters/controllers/transaction/getTransaction.controller.ts
@@ -0,0 +1,48 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { GetTransaction } from "../../../usecases/transaction/getTransaction";
+import { getTransactionInput } from "../schemas/transaction/getTransaction.schema";
+import { StatusCodes } from "http-status-codes";
+
+export class GetTransactionController {
+  private readonly useCase: GetTransaction;
+
+  constructor(useCase: GetTransaction) {
+    this.useCase = useCase;
+  }
+
+  async handle(request: FastifyRequest, reply: FastifyReply) {
+    const { id } = request.query as getTransactionInput;
+
+    try {
+      const {
+        categoryId,
+        date,
+        description,
+        id: transactionId,
+        isEntry,
+        title,
+        value,
+      } = await this.useCase.get(id);
+
+      await reply
+        .status(StatusCodes.OK)
+        .send({
+          data: {
+            categoryId,
+            date,
+            description,
+            transactionId,
+            isEntry,
+            title,
+            value,
+          },
+        });
+    } catch (e) {
+      const error = e as Error;
+
+      await reply
+        .status(StatusCodes.NOT_FOUND)
+        .send({ msg: error.message });
+    }
+  }
+}
