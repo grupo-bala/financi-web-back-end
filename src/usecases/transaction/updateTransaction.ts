2fff65583c487c0c396193cc847714dc85ccbc89
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-05-13 00:19:29 -0300

    feat: implement the update transaction use case

---
+++ src/usecases/transaction/updateTransaction.ts
@@ -0,0 +1,28 @@
+import { Transaction } from "../../model/transaction";
+import { CategoryRepository } from "../category/interface/categoryRepository";
+import { TransactionRepository } from "./interface/transactionRepository";
+
+export class UpdateTransaction {
+  private readonly transactionRepository: TransactionRepository;
+  private readonly categoryRepository: CategoryRepository;
+
+  constructor(
+    transactionRepository: TransactionRepository,
+    categoryRepository: CategoryRepository,
+  ) {
+    this.transactionRepository = transactionRepository;
+    this.categoryRepository = categoryRepository;
+  }
+
+  async update(transaction: Transaction): Promise<void> {
+    if (!await this.transactionRepository.existsById(transaction.id!)) {
+      throw new Error("Essa transação não existe");
+    } else if (
+      !await this.categoryRepository.existsById(transaction.categoryId)
+    ) {
+      throw new Error("Essa categoria não existe");
+    }
+
+    await this.transactionRepository.update(transaction);
+  }
+}
