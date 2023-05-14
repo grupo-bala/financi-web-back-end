3e6e2d5d4f485a8c0ff357c27dfc607482460d7a
Antônio Hugo <hugorplobo@gmail.com>
2023-05-14 18:39:20 -0300

    chore: delete failing migration

--- prisma/migrations/20230514195335_add_recommended_column_in_news/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `recommended` to the `news` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "news" ADD COLUMN     "recommended" BOOLEAN NOT NULL;
