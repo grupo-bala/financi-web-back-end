95ab408fcc081ae7a218ec255ae23913652d1401
Antônio Hugo <hugorplobo@gmail.com>
2023-05-27 18:41:11 -0300

    feat: add generate report usecase

--- src/usecases/transaction/interface/transactionRepository.ts
+++ src/usecases/transaction/interface/transactionRepository.ts
@@ -1,5 +1,8 @@
 import { Transaction } from "../../../model/transaction";
 import { TransactionPreview } from "../../../model/transactionPreview";
+import { Interval } from "../../statistics/data/Filter";
+
+export type TransactionCategory = Transaction & { category: string };
 
 export interface TransactionRepository {
   existsById: (id: number) => Promise<boolean>;
@@ -9,4 +12,8 @@ export interface TransactionRepository {
   remove: (id: number) => Promise<void>;
   getAllPreviews: (page: number, size: number) => Promise<TransactionPreview[]>;
   getSize: () => Promise<number>;
+  getByPeriod: (
+    userId: number,
+    interval: Interval
+  ) => Promise<TransactionCategory[]>;
 }
