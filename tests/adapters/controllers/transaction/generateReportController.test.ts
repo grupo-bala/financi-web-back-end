110aac6c70a697aba374ce1775e925e87c602a72
Antônio Hugo <hugorplobo@gmail.com>
2023-05-28 19:36:03 -0300

    test: add generate report tests

---
+++ tests/adapters/controllers/transaction/generateReportController.test.ts
@@ -0,0 +1,80 @@
+import Fastify from "fastify";
+import { registerHandlers } from "../../../../src/server/plugins/registerHandlers";
+import { Token } from "../../../../src/adapters/data/token";
+import { GenerateReport } from "../../../../src/usecases/transaction/generateReport";
+import { mock } from "../../../util";
+import { Interval } from "../../../../src/usecases/statistics/data/Filter";
+import { StatusCodes } from "http-status-codes";
+import { PDF } from "../../../../src/adapters/services/pdf";
+
+const server = Fastify();
+jest.mock("../../../../src/usecases/transaction/generateReport.ts");
+
+describe("testes do controller de relatórios", () => {
+  beforeAll(() => {
+    server.register(registerHandlers);
+  });
+
+  test("caso de uso com erros deve falhar com status 400", async () => {
+    const defaultId = 0;
+    const token = Token.encode(defaultId, false);
+
+    mock(GenerateReport).mockImplementation(() => {
+      return {
+        generate: async (_id: number, _interval: Interval) => {
+          throw new Error();
+        },
+      };
+    });
+
+    const response = await server.inject({
+      method: "GET",
+      url: "http://localhost/generate-report",
+      query: {
+        initDate: "2023-01-01",
+        endDate: "2023-05-30",
+      },
+      cookies: {
+        "financi-jwt": token.encoded,
+      },
+    });
+
+    expect(response.statusCode).toBe(StatusCodes.BAD_REQUEST);
+  });
+
+  test(
+    "caso de uso sem erros deve passar com status 200 e retornar um arquivo",
+    async () => {
+      const defaultId = 0;
+      const token = Token.encode(defaultId, false);
+
+      mock(GenerateReport).mockImplementation(() => {
+        return {
+          generate: async (_id: number, _interval: Interval) => {
+            return await PDF.generateReport([], "test");
+          },
+        };
+      });
+
+      const response = await server.inject({
+        method: "GET",
+        url: "http://localhost/generate-report",
+        query: {
+          initDate: "2023-01-01",
+          endDate: "2023-05-30",
+        },
+        cookies: {
+          "financi-jwt": token.encoded,
+        },
+      });
+
+      expect(response.statusCode).toBe(StatusCodes.OK);
+      expect(response.headers["content-disposition"]).toBe(
+        "attachment; filename=test.pdf",
+      );
+      expect(response.headers["content-type"]).toBe(
+        "application/octet-stream",
+      );
+    },
+  );
+});
---
+++ tests/usecases/transaction/generateReport.test.ts
@@ -0,0 +1,105 @@
+import Fastify from "fastify";
+import { registerHandlers } from "../../../src/server/plugins/registerHandlers";
+import { mock } from "../../util";
+import {
+  PostgresTransactionRepository,
+} from "../../../src/adapters/repositories/postgresTransactionRepository";
+import {
+  PostgresUserRepository,
+} from "../../../src/adapters/repositories/postgresUserRepository";
+import {
+  GenerateReport,
+} from "../../../src/usecases/transaction/generateReport";
+import { User } from "../../../src/model/user";
+import { Decimal } from "@prisma/client/runtime/library";
+import { Email } from "../../../src/model/data/email";
+import { Password } from "../../../src/model/data/password";
+import { Interval } from "../../../src/usecases/statistics/data/Filter";
+import path from "path";
+import fs from "fs/promises";
+
+jest.mock(
+  "../../../src/adapters/repositories/postgresTransactionRepository.ts",
+);
+jest.mock("../../../src/adapters/repositories/postgresUserRepository.ts");
+const server = Fastify();
+
+describe("testes do caso de uso de gerar relatório", () => {
+  beforeAll(() => {
+    server.register(registerHandlers);
+  });
+
+  test("data inicial maior que final deve falhar", async () => {
+    const usecase = new GenerateReport(
+      new PostgresTransactionRepository(),
+      new PostgresUserRepository(),
+    );
+    const id = 0;
+
+    await expect(usecase.generate(
+      id,
+      {
+        initDate: new Date("2023-05-30"),
+        endDate: new Date("2023-01-01"),
+      },
+    ))
+      .rejects
+      .toThrow();
+  });
+
+  test("data válida deve retornar caminho do arquivo", async () => {
+    const noMoney = 0;
+    const id = 0;
+
+    const returnUser = new User({
+      id,
+      name: "",
+      username: "test",
+      isAdmin: false,
+      fixedIncome: new Decimal(noMoney),
+      balance: new Decimal(noMoney),
+      email: new Email("test@gmail.com"),
+      password: Password.fromHash(""),
+    });
+
+    mock(PostgresUserRepository).mockImplementation(() => {
+      return {
+        getById: async (_id: number) => returnUser,
+      };
+    });
+
+    mock(PostgresTransactionRepository).mockImplementation(() => {
+      return {
+        getByPeriod: async (_id: number, _interval: Interval) => {
+          return [];
+        },
+      };
+    });
+
+    const usecase = new GenerateReport(
+      new PostgresTransactionRepository(),
+      new PostgresUserRepository(),
+    );
+
+    const filePath = await usecase.generate(
+      id,
+      {
+        initDate: new Date("2023-01-01"),
+        endDate: new Date("2023-05-30"),
+      },
+    );
+
+    const expectedPath = path.join(
+      process.cwd(),
+      "public/users/reports",
+      `${returnUser.username}.pdf`,
+    );
+
+    expect(expectedPath).toBe(filePath);
+    await expect(fs.stat(expectedPath))
+      .resolves
+      .not.toThrow();
+
+    await fs.unlink(expectedPath);
+  });
+});
