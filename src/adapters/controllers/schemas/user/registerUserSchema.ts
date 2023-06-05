c632f72bc71bd10f8fd370d7979332ccc34c5b57
Antônio Hugo <hugorplobo@gmail.com>
2023-06-05 14:01:40 -0300

    feat: add email and fixed income validations in register

--- src/adapters/controllers/schemas/user/registerUserSchema.ts
+++ src/adapters/controllers/schemas/user/registerUserSchema.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 
 export const registerUserSchema = z.object({
   name: z.string(),
-  email: z.string(),
+  email: z.string().email(),
   password: z.string(),
   username: z.string(),
-  fixedIncome: z.coerce.number(),
+  fixedIncome: z.coerce.number().positive(),
 });
 
 export type RegisterUserInput = z.infer<typeof registerUserSchema>;
