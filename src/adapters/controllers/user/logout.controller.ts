3aaad748fb0230239fa31bd82c52a4689502ed97
Antônio Hugo <hugorplobo@gmail.com>
2023-07-04 03:06:20 -0300

    fix: update return cookie on logout

--- src/adapters/controllers/user/logout.controller.ts
+++ src/adapters/controllers/user/logout.controller.ts
@@ -1,9 +1,27 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { EnviromentVars } from "../../../server/config/enviromentVars";
 
 export class LogoutController {
   async handle(request: FastifyRequest, reply: FastifyReply) {
-    await reply
-      .setCookie("financi-jwt", "")
-      .send();
+    if (EnviromentVars.vars.ENVIRONMENT === "debug") {
+      await reply
+        .setCookie("financi-jwt", "", {
+          httpOnly: true,
+          path: "/",
+          sameSite: "strict",
+          domain: EnviromentVars.vars.COOKIE_DOMAIN,
+        })
+        .send();
+    } else {
+      await reply
+        .setCookie("financi-jwt", "", {
+          httpOnly: true,
+          path: "/",
+          sameSite: "none",
+          secure: true,
+          domain: EnviromentVars.vars.COOKIE_DOMAIN,
+        })
+        .send();
+    }
   }
 }
